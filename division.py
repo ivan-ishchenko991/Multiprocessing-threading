4273430ffb06f5d1fd9b0e9eeceb3f43955736c6
ivan-ishchenko991 <lvan.lshchenko19.01@gmail.com>
2023-08-07 13:11:53 +0300

    Initial commit

---
+++ division.py
@@ -0,0 +1,35 @@
+import time
+from multiprocessing import Pool, cpu_count
+
+
+def factorize(*numbers: int) -> list[list[int]]:
+    result = []
+    for number in numbers:
+        res = []
+        for i in range(1, number + 1):
+            if number % i == 0:
+                res.append(i)
+        result.append(res)
+    return result
+
+if __name__ == "__main__":
+    test_numbers = [128, 255, 99999, 10651060]
+    start = time.time()
+    a, b, c, d = factorize(*test_numbers)
+
+    assert a == [1, 2, 4, 8, 16, 32, 64, 128]
+    assert b == [1, 3, 5, 15, 17, 51, 85, 255]
+    assert c == [1, 3, 9, 41, 123, 271, 369, 813, 2439, 11111, 33333, 99999]
+    assert d == [1, 2, 4, 5, 7, 10, 14, 20, 28, 35, 70, 140, 76079, 152158, 304316,
+                 380395, 532553, 760790, 1065106, 1521580, 2130212, 2662765, 5325530, 10651060]
+    finish = time.time()
+    print("Synchronous version:", finish - start)
+
+    print('-' * len(str(finish)) + '-' * 21)
+
+    start_2 = time.time()
+
+    with Pool(cpu_count()) as p:
+        p.map(factorize, test_numbers)
+    finish_2 = time.time()
+    print("Parallel execution:", finish_2 - start_2)
